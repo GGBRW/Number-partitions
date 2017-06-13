3b60ce4c37d337bb2e674c90beec63635e83dc41
Gees Brouwer <brouwer.gees@gmail.com>
2017-06-13 22:24:03 +0200

    Create main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function c(num,it) {
-  let combinations = [];
-  (f = (num,it,pos = 1, list = []) => {
+  const combinations = [];
+  (const f = (num,it,pos = 1,list = []) => {
     for(let i = Math.max(0, num - 7 * (it - pos)); i <= Math.min(num,7); ++i) {
       it == pos ? combinations.push([...list,i].join('')) : f(num-i,it,pos+1,[...list,i])
     }
